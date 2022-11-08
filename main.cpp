7f37782d3c21af39cb139e544c6a441fd85febf8
madisonnicolej <117469674+madisonnicolej@users.noreply.github.com>
2022-11-08 10:24:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,16 +17,16 @@ int main()
 	root1 = (-b + sqrt(discriminant)) / (2 * a);
 	root2 = (-b - sqrt(discriminant)) / (2 * a);
 
-	if (discriminant > 0) {
-		cout << "There are two real roots." << endl;
-		cout << "-First root " << root1 << endl;
-		cout << "-Second root " << root2 << endl;
+	if (discriminant < 0) {
+		cout << "The roots are complex." << endl;
 	}
 	else if (discriminant == 0) {
 		cout << "The single root is " << root1 << endl;
 	}
 	else {
-		cout << "The roots are complex." << endl;
+		cout << "There are two real roots." << endl;
+		cout << "-First root " << root1 << endl;
+		cout << "-Second root " << root2 << endl;
 	}
 	
 	return 0;
